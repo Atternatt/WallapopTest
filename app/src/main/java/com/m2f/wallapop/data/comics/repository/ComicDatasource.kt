4ce191edbe9220a9c9bb7aaee8841fdaf9e41b8a
Marc <ignis.fatue@gmail.com>
2017-04-15 19:25:52 +0200

    added ComicRepository Implementation

---
+++ app/src/main/java/com/m2f/wallapop/data/comics/repository/ComicDatasource.kt
@@ -0,0 +1,13 @@
+package com.m2f.wallapop.data.comics.repository
+
+import com.m2f.wallapop.domain.comic.model.ComicList
+import io.reactivex.Flowable
+
+/**
+ * @author Marc Moreno
+ * @version 1
+ */
+interface ComicDatasource {
+
+    fun getComicsForCharacter(characterId: Int): Flowable<ComicList>
+}
---
+++ app/src/main/java/com/m2f/wallapop/data/comics/repository/ComicRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.m2f.wallapop.data.comics.repository
+
+import com.m2f.wallapop.domain.comic.model.ComicList
+import com.m2f.wallapop.domain.comic.repository.ComicRepository
+import io.reactivex.Flowable
+import javax.inject.Inject
+
+/**
+ * @author Marc Moreno
+ * @version 1
+ */
+class ComicRepositoryImpl
+@Inject constructor(vararg val comicDatasource: ComicDatasource): ComicRepository {
+
+    override fun getComicsForCharacter(characterId: Int): Flowable<ComicList> {
+        return Flowable.merge(comicDatasource.map { it.getComicsForCharacter(characterId) })
+                .first(ComicList(0, "", listOf()))
+                .toFlowable()
+    }
+}
